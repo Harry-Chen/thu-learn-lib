4214f45e74cc0cd1d7ec1e414cf35cd2cde8ba31
mayeths <wow@huanghope.com>
2020-01-21 10:35:11 +0800

    Add simple test case, including login & logout
    - Use jest to test helper. Avoid accidentally modifying
      the original function.

---
+++ test/v1.test.ts
@@ -0,0 +1,101 @@
+import { Learn2018Helper } from "../src"
+import * as dotenv from "dotenv"
+
+dotenv.config({ path: "test/.env" })
+const U = process.env.U!;  // username
+const P = process.env.P!;  // password
+//                     ^ note the exclamation mark here
+// prevent TS2322: Type 'string | undefined' is not assignable to type 'string'.
+
+describe('helper login & logout', () => {
+
+  it("should login & logout correctly if account is right", async () => {
+    const helper = new Learn2018Helper();
+    const loginok = await helper.login(U, P);
+    expect(loginok).toEqual(true);
+    const logoutok = await helper.logout();
+    expect(logoutok).toEqual(false);  //false if logout correctly.
+  })
+
+  it("should failed to login if account is incorrect", async () => {
+    const helper = new Learn2018Helper();
+    const loginok = await helper.login("nouser", "nopass");
+    expect(loginok).toEqual(false);
+  })
+
+  it("should throw error if hasn't login", async () => {
+    const helper = new Learn2018Helper();
+    await expect(helper.getSemesterIdList()).rejects.toThrow("Not logged in.");
+  })
+})
+
+
+describe('helper basic function', () => {
+  let helper: Learn2018Helper;
+  let semesterTester: string;
+  let courseTester: string;
+
+  beforeAll(async () => {
+    const _h = new Learn2018Helper();
+    const loginok = await _h.login(U, P);
+    expect(loginok).toEqual(true);
+    const currSemester = await _h.getCurrentSemester();
+    const courses = await _h.getCourseList(currSemester.id);
+    expect(courses.length).toBeGreaterThan(0);
+    semesterTester = currSemester.id;
+    courseTester = courses[0].id;
+  })
+  beforeAll(async () => {
+    helper = new Learn2018Helper();
+    await helper.login(U, P);
+  })
+  afterAll(async () => {
+    await helper.logout();
+  })
+
+  it("should get semesterIdList correctly", async () => {
+    const semesters = await helper.getSemesterIdList();
+    expect(Array.isArray(semesters)).toEqual(true);
+    expect(semesters[0]).toBeNull();  // [0] is null
+    expect(typeof semesters[1]).toBe("string");
+    expect(semesters[1]).toEqual(semesterTester);
+  })
+
+  it("should get currentSemester correctly", async () => {
+    const currSemester = await helper.getCurrentSemester();
+    expect(currSemester).not.toBeUndefined();
+    expect(currSemester).not.toBeNull();
+    expect(currSemester.id).toEqual(semesterTester);
+  })
+
+  it("should get courseList correctly", async () => {
+    const courses = await helper.getCourseList(semesterTester);
+    expect(courses.length).toBeGreaterThan(0);
+    expect(courses[0].id).toEqual(courseTester);
+  })
+
+  it("should get TAcourses correctly", async () => {
+    expect("TobeDone").toEqual("TobeDone");
+  })
+
+  it("should get discussionList correctly", async () => {
+    expect("TobeDone").toEqual("TobeDone");
+  })
+
+  it("should get NotificationList correctly", async () => {
+    expect("TobeDone").toEqual("TobeDone");
+  })
+
+  it("should get FileList correctly", async () => {
+    expect("TobeDone").toEqual("TobeDone");
+  })
+
+  it("should get HomeworkList correctly", async () => {
+    expect("TobeDone").toEqual("TobeDone");
+  })
+
+  it("should get AnsweredQuestionList correctly", async () => {
+    expect("TobeDone").toEqual("TobeDone");
+  })
+
+})

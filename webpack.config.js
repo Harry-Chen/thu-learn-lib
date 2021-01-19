09a41549bb32be2ae2ce612d2b79393d6ff70373
Harry Chen <i@harrychen.xyz>
2021-01-19 14:36:37 +0800

    Upgrade to webpack 5
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,42 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
 const htmlPlugin = new HtmlWebpackPlugin({
-  template: "./demo/index.html",
-  filename: "./index.html"
+  template: './demo/index.html',
+  filename: './index.html',
 });
 
 module.exports = {
   entry: {
-    index: "./demo/index.ts",
+    index: './demo/index.ts',
   },
   output: {
-    path: path.resolve("./dist"),
-    filename: "[name].js"
+    path: path.resolve('./dist'),
+    filename: '[name].js',
   },
-  plugins: [htmlPlugin],
+  plugins: [
+    htmlPlugin,
+    new NodePolyfillPlugin(),
+  ],
   devServer: {
     contentBase: './dist',
     headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
+      'Access-Control-Allow-Origin': '*',
+    },
   },
   module: {
     rules: [
       {
         exclude: /node_modules/,
         test: /\.tsx?$/,
-        use: "ts-loader"
-      }
-    ]
+        use: 'ts-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: ['.ts', '.tsx', '.js'],
   },
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
 };

8ab4a3238b832f9a99aa97d87de792cc9e6a4bb8
Harry Chen <i@harrychen.xyz>
2021-09-14 11:06:39 +0800

    Add more plugins to Webpack to simplify building
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,36 @@
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
+import RemovePlugin from 'remove-files-webpack-plugin';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const htmlPlugin = new HtmlWebpackPlugin({
   template: './demo/index.html',
   filename: './index.html',
+});
+
+const copyPolyFill = new CopyPlugin({
+  patterns: [
+    { from: './node_modules/webextension-polyfill/dist/browser-polyfill.min.js', to: './' },
+  ],
+});
+
+const removeRedundantFile = new RemovePlugin({
+  // remove output dist files
+  before: {
+    include: [
+      './dist/index.html',
+      './dist/browser-polyfill.min.js',
+      './dist/index.js',
+    ],
+  },
+  // remove .d.ts files produced by Webpack to source directories
+  after: {
+    include: ['./lib/src', './lib/demo'],
+  },
 });
 
 export default {
@@ -19,8 +42,10 @@ export default {
     filename: '[name].js',
   },
   plugins: [
-    htmlPlugin,
     new NodePolyfillPlugin(),
+    htmlPlugin,
+    copyPolyFill,
+    removeRedundantFile,
   ],
   devServer: {
     contentBase: './dist',

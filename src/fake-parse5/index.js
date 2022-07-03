ff15304980eebf98b70074bda80d1d52ce02f403
Harry Chen <i@harrychen.xyz>
2022-07-03 15:00:26 +0800

    Fix fake-parse5 code
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- src/fake-parse5/index.js
+++ src/fake-parse5/index.js
@@ -1,5 +1,8 @@
-export default {
-    parse: () => {},
-    parseFragment: () => {},
-    serialize: () => {},
-};
+/// provide empty parse5 implementation to reduce bundled .js size
+
+// parse5-htmlparser2-tree-adapter
+export const adapter = () => {};
+// parse5
+export const parse = () => {};
+export const parseFragment = () => {};
+export const serializeOuter = () => {};
